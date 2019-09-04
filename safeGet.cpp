ed24e0bd09671238a2ad6a3ed4d6ad6164c32e50
Roy Bellingan <roy@tech.chameleon.ad>
2019-09-04 13:52:26 +0000

    fix default value

--- safeGet.cpp
+++ safeGet.cpp
@@ -1,35 +1,31 @@
 #include "safeGet.h"
 
-rapidjson::GenericValue<rapidjson::UTF8<> > zeroStringInit(){
+rapidjson::GenericValue<rapidjson::UTF8<>> zeroStringInit() {
 	rapidjson::GenericValue<rapidjson::UTF8<>> v;
 	v.SetString("0");
 	return v;
 }
 
-rapidjson::GenericValue<rapidjson::UTF8<> > emptyStringInit(){
+rapidjson::GenericValue<rapidjson::UTF8<>> emptyStringInit() {
 	rapidjson::GenericValue<rapidjson::UTF8<>> v;
-	v.SetString("0");
+	v.SetString("");
 	return v;
 }
 
-
-rapidjson::GenericValue<rapidjson::UTF8<> > zeroInit(){
+rapidjson::GenericValue<rapidjson::UTF8<>> zeroInit() {
 	rapidjson::GenericValue<rapidjson::UTF8<>> v;
 	v.SetDouble(0);
 	return v;
 }
 
-const rapidjson::GenericValue<rapidjson::UTF8<>> rapidfunkz::ZeroString = zeroStringInit();
+const rapidjson::GenericValue<rapidjson::UTF8<>> rapidfunkz::ZeroString  = zeroStringInit();
 const rapidjson::GenericValue<rapidjson::UTF8<>> rapidfunkz::EmptyString = emptyStringInit();
-const rapidjson::GenericValue<rapidjson::UTF8<>> rapidfunkz::Zero = zeroInit();
+const rapidjson::GenericValue<rapidjson::UTF8<>> rapidfunkz::Zero        = zeroInit();
 
-const rapidjson::GenericValue<rapidjson::UTF8<> >& rapidfunkz::safeGet(const rapidjson::GenericValue<rapidjson::UTF8<> > &line, const char *name, const rapidjson::GenericValue<rapidjson::UTF8<> > &defaultVal) {
+const rapidjson::GenericValue<rapidjson::UTF8<>>& rapidfunkz::safeGet(const rapidjson::GenericValue<rapidjson::UTF8<>>& line, const char* name, const rapidjson::GenericValue<rapidjson::UTF8<>>& defaultVal) {
 	auto iter = line.FindMember(name);
 	if (iter == line.MemberEnd()) {
 		return defaultVal;
 	}
 	return iter->value;
 }
-
-
-
