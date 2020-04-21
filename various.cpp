e2d2a5ba2293f66b12af1d8f1ff9940d0adb346b
Roy Bellingan <roy@tech.techadsmedia.com>
2020-04-21 01:33:14 +0100

    print type

---
+++ various.cpp
@@ -0,0 +1,28 @@
+#include "various.h"
+
+using namespace rapidjson;
+QString printType(rapidjson::Type t) {
+	switch (t) {
+	case Type::kNullType:
+		return "null";
+		break;
+	case Type::kFalseType:
+		return "bool (false)";
+		break;
+	case Type::kTrueType:
+		return "bool (true)";
+		break;
+	case Type::kObjectType:
+		return "object";
+		break;
+	case Type::kArrayType:
+		return "array";
+		break;
+	case Type::kStringType:
+		return "string";
+		break;
+	case Type::kNumberType:
+		return "number";
+		break;
+	}
+}
---
+++ various.h
@@ -0,0 +1,9 @@
+#ifndef VARIOUS_H
+#define VARIOUS_H
+
+#include <QString>
+#include "rapidjson/includeMe.h"
+
+QString printType(rapidjson::Type t);
+
+#endif // VARIOUS_H
